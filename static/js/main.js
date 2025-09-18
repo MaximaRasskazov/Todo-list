3678141a172872ec392b4204ce4c183e3a17735d
Maxima <MaximaRasskazov@gmail.com>
2025-09-18 19:34:10 +0500

    Новая структура

---
+++ static/js/main.js
@@ -0,0 +1,11 @@
+// main.js
+document.addEventListener('DOMContentLoaded', function() {
+    // Инициализация календаря
+    initCalendar();
+    
+    // Инициализация заметок
+    initNotes();
+    
+    // Инициализация туду-листа (уже существующий код)
+    initTodo();
+});
